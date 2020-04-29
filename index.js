f9f7c9c3e079b1682187d5d2db5380aedc17e0ba
shrimpseaweed <shrimpseaweed@gmail.com>
2020-04-29 17:03:59 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(()=>{
+    const max = 10;
+    let i=0;
+    setTimeout(function loop(){
+        console.log(i++);
+        const elem = document.createElement('p');
+        elem.innerHTML = i;
+        document.body.appendChild(elem);
+        if(i<max) setTimeout(loop, 1000);
+    }, 1000);
+})();
